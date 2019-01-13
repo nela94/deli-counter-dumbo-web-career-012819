e0a11f9df3e322c3e65a6519474894928e2539bb
Manuela  Sanchez <m.sanchez947@hotmail.com>
2019-01-13 03:08:41 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,5 +1,5 @@
 def line(current_line)
-   if katz_deli = []
+   if katz_deli.empty?
      puts "The line is currently empty."
    else
      puts "The line is currently: 1.#{katz_deli} 2.#{katz_deli} 3.#{katz_deli} 4. #{katz_deli} 5.#{katz_deli}"
