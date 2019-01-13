13429e4edbbb1f1aee35bb196df7c1e50dbd96f0
Manuela  Sanchez <m.sanchez947@hotmail.com>
2019-01-13 02:41:03 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,8 +1,4 @@
-def line(katz_deli)
-  katz_deli = []
-  puts "The line is currently empty."
-end
-def line(other_deli)
+def line(current_line)
   other_deli = ["Logan", "Avi", "Spencer"]
   other_deli.each_with_index{|value, index| puts "The line is currently: #{index}.#{value}"}
 end
