dd4baa168d3a298a86cf8a897d1eab9cf0d4b98c
Manuela  Sanchez <m.sanchez947@hotmail.com>
2019-01-13 02:44:38 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,4 +1,4 @@
 def line(current_line)
   other_deli = ["Logan", "Avi", "Spencer"]
-  other_deli.each_with_index{|value, index| puts "The line is currently: #{index}.#{value}"}
+  other_deli.push(1..3)
 end
