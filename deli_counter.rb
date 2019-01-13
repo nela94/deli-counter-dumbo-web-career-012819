d60105294de2b093a3896004a3f1ed1f22fdcc3f
Manuela  Sanchez <m.sanchez947@hotmail.com>
2019-01-13 03:17:42 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,6 +2,10 @@ def line(katz_deli)
    if katz_deli.empty?
      puts "The line is currently empty."
    else
-
+     current_line = "The line is currently:"
+     katz_deli.each_with_index(1) do |customer, i|
+       current_line.push("#{i}. #{customer}")
 end
+end
+puts current_line
 end
