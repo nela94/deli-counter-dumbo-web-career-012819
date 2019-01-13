ad7007f5537bd53252a8e12760616bf607473218
Manuela  Sanchez <m.sanchez947@hotmail.com>
2019-01-13 03:20:27 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@ def line(katz_deli)
    else
      current_line = "The line is currently:"
      katz_deli.each_with_index(1) do |customer, i|
-       current_line.push("#{i}. #{customer}")
+       current_line << "#{i}. #{customer}"
 end
 end
  current_line
